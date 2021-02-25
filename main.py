4ffddf9e558bd5c28aed451d5c93095dac0262c5
Yoann Dufresne <yoann.dufresne0@gmail.com>
2021-02-25 19:45:04 +0100

    e

--- main.py
+++ main.py
@@ -22,12 +22,58 @@ def look_for_best_score():
       overall_best_score = file_score
 
 
+# parsing
+D, I, S, V, F = 0, 0, 0, 0, 0
+streets = []
+paths = []
+intersections = {}
+def parse(filename):
+    lines = open(filename).readlines()
+    global D,I,S,V,F
+    D,I,S,V,F = map(int,lines[0].split())
+   
+    global streets
+    streets = []
+    for line in lines[1:S+1]:
+        assert(len(line.split()) == 4)
+        B,E, name, L = line.split()
+        B,E,L=map(int,[B,E,L])
+        streets += [(B,E,name,L)]
+    assert(len(streets) == S)
+
+    global intersections
+    for B, E, name, _ in streets:
+      if B not in intersections:
+        intersections[B] = [], []
+      intersections[B][1].append(name)
+
+      if E not in intersections:
+        intersections[E] = [], []
+      intersections[E][0].append(name)
+
+       
+    global paths
+    for line in lines[S+1:]:
+        P = int(line.split()[0])
+        assert(len(line.split()) == P+1)
+        paths += [line.split()[1:]]
+    assert(len(paths) == V)
+parse(sys.argv[1])
+print(sys.argv[1],D,"secs",I,"intersections",S,"streets",V,"cars",F,"bonus",sep='\t')
+print(streets)
+print(paths)
+
+
+
+# smart things
 
 class Solution:
 
   def __init__(self):
     self.score = 0
     self.best_score = 0
+
+    self.cycles = {}
 
   def get_score(self):
     return self.score
@@ -47,9 +93,13 @@ class Solution:
 
 
 def naive_solution():
+  global intersections
   sol = Solution()
-  sol.score = random.randint(0, 100)
 
+  for i, tup in intersections.items():
+    sol.cycles[i] = [(x, 1) for x in tup[0]]
+
+  print(sol.cycles)
   return sol
 
 
@@ -57,7 +107,7 @@ def naive_solution():
 
 def main():
   sol = naive_solution()
-  sol.save()
+  # sol.save()
 
 
 
